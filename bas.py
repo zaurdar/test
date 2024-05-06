4faf28348d2e85f88847fd688161d572b2f7a912
gabriel <hugo.musin@gmail.com>
2024-05-06 12:56:43 +0200

    n

--- bas.py
+++ bas.py
@@ -30,10 +30,10 @@ def app():
     st.header("calibrage")
     x = st.slider("coordonnée x ", 0, 1920)
     y = st.slider("coordonnée y ", 0, 1080)
-    temp_dir = tempfile.mkdtemp()
+    temp = tempfile.mkdtemp()
     st.image("CVM3D/fond/fond_test.jpg")
     if st.button("enter"):
-        temp_dir = os.path.join(path,temp_dir)
+        temp_dir = os.path.join(path,temp)
         st.write(temp_dir)
         test_fond_cmd = 'ffmpeg -i CVM3D/fond/fond_test.jpg -vf scale=1920:1080 ' + str(temp_dir) + '/fond.jpg -y'
 
