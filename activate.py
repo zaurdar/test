94909ddc91272d5c38c057ff123d5ff8c9e737b5
gabriel <hugo.musin@gmail.com>
2024-05-06 12:05:43 +0200

    n

--- activate.py
+++ activate.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import subprocess
+import os
+import sys
 def main():
     st.title("Page de connexion")
-
+    chemin_script = os.path.abspath(sys.argv[0])
+    path = os.path.dirname(chemin_script)
+    st.write(path)
     # Champ de saisie pour le nom d'utilisateur
     username = st.text_input("Nom d'utilisateur")
 
@@ -13,7 +17,7 @@ def main():
     if st.button("Se connecter"):
         if username == "moi" and password == "malice":
             st.success("Connexion réussie !")
-            subprocess.run("streamlit run app.py")
+            subprocess.run("streamlit run "+str(path)+"app.py")
         else:
             st.error("Nom d'utilisateur ou mot de passe incorrect")
 
